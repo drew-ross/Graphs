fd16d41e2572a50e108c49fa327de660fbfe869d
Drew Ross <mail.drew.ross@gmail.com>
2020-10-05 15:12:11 -0700

    Complete bfs, test manually.

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -52,12 +52,17 @@ class Graph:
                 self.dft_recursive(node, visited)
 
     def bfs(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing the shortest path from
-        starting_vertex to destination_vertex in
-        breath-first order.
-        """
-        pass  # TODO
+        visited = set()
+        q = []
+        q.append([starting_vertex])
+        while q:
+            v = q.pop(0)
+            if v[-1] == destination_vertex:
+                return v
+            if v[-1] not in visited:
+                visited.add(v[-1])
+                for n in self.vertices[v[-1]]:
+                    q.append(v + [n])
 
     def dfs(self, starting_vertex, destination_vertex):
         """
@@ -119,8 +124,8 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
-    graph.bft(1)
-    print('')
+    # graph.bft(1)
+    # print('')
 
     '''
     Valid DFT paths:
@@ -129,14 +134,14 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
-    graph.dft(1)
+    # graph.dft(1)
     # graph.dft_recursive(1)
 
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    # print(graph.bfs(1, 6))
+    print(graph.bfs(1, 6))
 
     '''
     Valid DFS paths:
