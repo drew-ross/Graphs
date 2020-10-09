805e6efd1f6bb7fa245a183a0f18aa23febe2adf
Drew Ross <mail.drew.ross@gmail.com>
2020-10-08 17:50:03 -0700

    Complete algorithm with looping.

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -17,11 +17,11 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
-world.print_rooms()
+# world.print_rooms()
 
 player = Player(world.starting_room)
 
@@ -29,7 +29,75 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# Fill traversal path ##############################
 
+
+def get_room(room, direction):
+    if direction in room_graph[room][1]:
+        return room_graph[room][1][direction]
+
+
+def reverse_direction(direction):
+    if direction == "n":
+        return "s"
+    if direction == "e":
+        return "w"
+    if direction == "s":
+        return "n"
+    if direction == "w":
+        return "e"
+
+
+def go(room, direction, path, backing=False):
+    next_room = get_room(room, direction)
+    room_map[room][direction] = next_room
+    if next_room is not None:
+        # move to the room, map exits for each room, keep a backup trail, log to visited
+        room_map[next_room][reverse_direction(direction)] = room
+        path.append(direction)
+        visited.add(next_room)
+        if not backing:
+            backup.append(reverse_direction(direction))
+        return next_room
+    else:
+        return room
+
+
+ways = ["n", "e", "s", "w"]
+possible_paths = []
+
+for x in range(10):
+    path = []
+    room_map = dict()
+
+    for i in range(len(room_graph)):
+        room_map[i] = {"n": "?", "e": "?", "s": "?", "w": "?"}
+
+    visited = set()
+    backup = []
+    room = 0
+    visited.add(room)
+
+    while len(visited) < 500:
+        if "?" in room_map[room].values():
+            if room_map[room][ways[0]] == "?":
+                room = go(room, ways[0], path)
+            elif room_map[room][ways[1]] == "?":
+                room = go(room, ways[1], path)
+            elif room_map[room][ways[2]] == "?":
+                room = go(room, ways[2], path)
+            else:
+                room = go(room, ways[3], path)
+        else:
+            back = backup[-1]
+            backup.pop()
+            room = go(room, back, path, True)
+
+    possible_paths.append(path)
+
+for path in possible_paths:
+    print(len(path))
+#################################
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -41,22 +109,23 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited"
+    )
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
